2580def959380a03e84158a59dd9046d1d65f644
Lucas Gabriel <lucasgab.lggs@gmail.com>
2025-04-19 11:26:10 -0300

    chore: início das configurações de segurança

---
+++ backend/src/main/java/br/ufrn/imd/cbm/config/SecurityConfig.java
@@ -0,0 +1,9 @@
+package br.ufrn.imd.cbm.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+}
