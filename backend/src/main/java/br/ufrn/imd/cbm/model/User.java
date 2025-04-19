af0b4199db368fea2f633fe66ed1cbee946003b3
Lucas Gabriel <lucasgab.lggs@gmail.com>
2025-04-19 10:55:11 -0300

    chore: configuração do banco de dados

--- backend/src/main/java/br/ufrn/imd/cbm/model/User.java
+++ backend/src/main/java/br/ufrn/imd/cbm/model/User.java
@@ -1,12 +1,27 @@
 package br.ufrn.imd.cbm.model;
 
 import br.ufrn.imd.cbm.enums.Role;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
 
+@Entity
+@Table(name = "users")
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(nullable = false)
     private String name;
+
+    @Column(unique = true, nullable = false)
     private String email;
+
+    @JsonIgnore
     private String password;
+
+    @Enumerated(EnumType.STRING)
     private Role role;
 
     public Long getId() {
