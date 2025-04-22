3144e4d0f203d2703a978a7fed43e75b7fb3da01
Allan073 <allanvictorv7@gmail.com>
2025-04-21 21:29:57 -0300

    Add files via upload

---
+++ backend/src/main/java/br/ufrn/imd/cbm/repositories/RoleRepository.java
@@ -0,0 +1,12 @@
+package br.ufrn.imd.cbm.repositories;
+
+import br.ufrn.imd.cbm.models.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+
+public interface RoleRepository extends JpaRepository<Role, Long>  {
+    
+}
