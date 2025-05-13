471d0b31d023f8fb0ec8fbc8ae23fb83db5efc55
Allan073 <allanvictorv7@gmail.com>
2025-05-12 22:10:10 -0300

    Add files via upload

---
+++ backend/src/main/java/br/ufrn/imd/cbm/repositories/ClientRepository.java
@@ -0,0 +1,12 @@
+package br.ufrn.imd.cbm.repositories;
+
+import br.ufrn.imd.cbm.models.Client;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface ClientRepository extends JpaRepository<Client, Long>  {
+    
+}
