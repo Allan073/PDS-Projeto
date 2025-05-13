52fdd10e410cb26aa355bc82e6cccb9f44403a3a
Allan073 <allanvictorv7@gmail.com>
2025-05-13 11:03:38 -0300

    Update FinanceRepository.java

--- backend/src/main/java/br/ufrn/imd/cbm/repositories/FinanceRepository.java
+++ backend/src/main/java/br/ufrn/imd/cbm/repositories/FinanceRepository.java
@@ -8,5 +8,5 @@ import java.util.Optional;
 
 @Repository
 public interface FinanceRepository extends JpaRepository<Finance, Long>  {
-    
+    List<Finance> findByTipo(String Tipo);  //adicionado por Allan
 }
