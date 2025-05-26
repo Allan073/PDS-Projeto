84d516704116c68614e812d1e7c827bfc17de989
artur <artur.nobre.114@ufrn.edu.br>
2025-05-26 02:31:04 -0300

    A annotation não funciona 10239812031 mortos 130418410 feridos

--- backend/src/main/java/br/ufrn/imd/cbm/configs/SecurityConfiguration.java
+++ backend/src/main/java/br/ufrn/imd/cbm/configs/SecurityConfiguration.java
@@ -22,7 +22,7 @@ public class SecurityConfiguration {
     private UserAuthenticationFilter userAuthenticationFilter;
 
     // Endpoints públicos
-    public static final String [] ENDPOINTS_WITH_AUTHENTICATION_NOT_REQUIRED = {
+   public static final String [] ENDPOINTS_WITH_AUTHENTICATION_NOT_REQUIRED = {
             "/users/login",
             "/users"
     };
