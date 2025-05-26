f0710a0457f81079a0095d7fa5888a8d9e505d26
Allan073 <allanvictorv7@gmail.com>
2025-05-26 13:23:13 -0300

    Create Agente.java

---
+++ backend/src/main/java/br/ufrn/imd/cbm/ai/Agente.java
@@ -0,0 +1,56 @@
+package br.ufrn.imd.cbm.ai;
+
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Component;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@Component
+public class Agent {
+    private String key;
+
+    public Agent(Environment env) {
+        this.key = env.getProperty("ai.key");
+    };
+
+    public void report() {
+        HttpClient client = HttpClient.newHttpClient();
+        ObjectMapper mapper = new ObjectMapper();
+
+        String prompt = "óla mundo";
+        Map<String, Object> map = Map.of(
+            "model", "google/gemini-2.0-flash-exp:free",
+            "messages", List.of(
+                Map.of(
+                    "role","user",
+                    "content", prompt
+                )
+            )
+        );
+
+        try {
+            String body = mapper.writeValueAsString(map);
+            HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("https://openrouter.ai/api/v1/chat/completions"))
+                .header("Content-Type", "application/json")
+                .header("Authorization", "Bearer " + this.key)
+                .POST(HttpRequest.BodyPublishers.ofString(body))
+                .build();
+            
+            client.sendAsync(request, HttpResponse.BodyHandlers.ofString())
+                .thenApply(HttpResponse::body)
+                .thenAccept((response) -> {
+                    System.out.println(response);
+                });
+        } catch (Exception e) {
+            // TODO: handle exception
+        };
+    };
+}
