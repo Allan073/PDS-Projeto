df69778036eb5f55ab59c265553b3294b06b4ce4
Allan073 <allanvictorv7@gmail.com>
2025-05-26 13:13:37 -0300

    Update FinanceReportController.java

--- backend/src/main/java/br/ufrn/imd/cbm/controllers/FinanceReportController.java
+++ backend/src/main/java/br/ufrn/imd/cbm/controllers/FinanceReportController.java
@@ -1,19 +1,29 @@
-import org.springframework.web.bind.annotation.GetMapping;
+package br.ufrn.imd.cbm.controllers;
+
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.http.ResponseEntity;
+
 import org.springframework.beans.factory.annotation.Autowired;
 
-import br.ufrn.imd.cbm.services.FinanceReportService; 
+import br.ufrn.imd.cbm.ai.Agent;
+import br.ufrn.imd.cbm.services.FinanceReportService;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @RestController
 @RequestMapping("/report")
 public class FinanceReportController {
+    //rota para testar a IA
+    
     @Autowired
     private FinanceReportService service;
 
+    @Autowired
+    private Agent agent;
+
     @GetMapping
-    public ResponseEntity<String> getReport() {
-        return ResponseEntity.ok(service.generateReport());
+    public String getMethodName() {
+        agent.report();
+        return "";
     }
 }
